f893089c3ab97c85737b75e46603cab24c49d773
Erik <erik.zilliacus@gmail.com>
2018-12-27 23:58:46 +0200

    peak detection for most important harmonics implemented. Tested. Basic functionality works, but sound not satisfactory. TODO: troubleshoot.

--- sound_analysis.py
+++ sound_analysis.py
@@ -1,21 +1,38 @@
 #import the pyplot and wavfile modules 
 import argparse
 import utils
+import glob
+import librosa
+import numpy as np
+
+SPECTRUM_CSV_DEST = "../../C++/stk_adaptive_synth/src/synth_files/"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--spectro", "-sp", action="store_true", help="Spectrogram")
 parser.add_argument("--load_file", "-lf", type=str,default=None, help="Name of wav file to analyze")
+parser.add_argument("--peaks", "-p", action="store_true", help="Peak detection on spectrogram")
 parser.add_argument("--save_file", "-sf", type=str,default=None, help="Name of csv filepath to upload data")
 args = parser.parse_args()
-
 def main():
 	if(args.spectro):
 		if(args.load_file != None):
-			spectrum,freqs = utils.spectrogram(args.load_file)
+			filepath = glob.glob('**/'+args.load_file,recursive=True) # Search for filename in current and all subdirectories
+			if(len(filepath) == 0): raise ValueError(args.load_file+ ' not found!')
+			spectrum,freqs = utils.spectrogram(filepath[0])
 		else:
 			raise Exception("--load_file argument missing!")
 		if(args.save_file != None):
-			utils.write_spectro(spectrum,freqs,args.save_file)
+			utils.write_spectro(spectrum,freqs,SPECTRUM_CSV_DEST+args.save_file)
+	if(args.peaks):
+		if(args.load_file != None):
+			filepath = glob.glob('**/'+args.load_file,recursive=True) # Search for filename in current and all subdirectories
+			if(len(filepath) == 0): raise ValueError(args.load_file+ ' not found!')
+			spectrum,freqs = utils.spectrogram(filepath[0],librosa_=True,plot=False)
+			peaks,amps = utils.get_peaks(spectrum,freqs,plot=True)
+			if(args.save_file != None):
+				utils.write_peaks(peaks,amps,SPECTRUM_CSV_DEST+args.save_file)
+		else:
+			raise Exception("--load_file argument missing!")
 
 if __name__ == '__main__':
 	main()
--- utils.py
+++ utils.py
@@ -1,43 +1,70 @@
 import glob
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from scipy.io import wavfile
+from scipy.signal import find_peaks,peak_prominences
 import numpy as np
+import librosa
+import librosa.display
 
-def spectrogram(filename):
-	sns.set()
-	sns.set_style("whitegrid", {'axes.grid' : False})
-	# Search for filename in current and all subdirectories
-	filepath = glob.glob('**/'+filename,recursive=True)
-	if(len(filepath) == 0): raise ValueError(filename + ' not found!')
+sns.set()
+sns.set_style("whitegrid", {'axes.grid' : False})
 
-	# Read the wav file (mono)
-	samplingFrequency, signalData = wavfile.read(filepath[0])
+MIN_PROMINENCE = 9 # Minimum prominence required for a frequency to be considered a harmonic. Used in peak detection
 
-	# Plot the signal read from wav file
-	plot.subplot(211)
-	plot.title('Spectrogram of ' + filename)
-	plot.plot(signalData)
-	plot.xlabel('Sample')
-	plot.ylabel('Amplitude')
-	plot.subplot(212)
-	spectrum,freqs,_,_ = plot.specgram(signalData,Fs=samplingFrequency)#,scale='dB')
-	plot.xlabel('Time')
-	plot.ylabel('Frequency')
-	plot.show()
+def spectrogram(filename,librosa_=True,plot=True):
+	if(not librosa_):
+		# Read the wav file (mono)
+		samplingFrequency, signalData = wavfile.read(filepath[0])
+		spectrum,freqs,_,_ = plot.specgram(signalData,Fs=samplingFrequency)#,scale='dB')
+		# Plot the signal read from wav file
+		if(plot):
+			plt.subplot(211)
+			plt.title('Spectrogram of ' + filename)
+			plt.plot(signalData)
+			plt.xlabel('Sample')
+			plt.ylabel('Amplitude')
+			plt.subplot(212)
+			plt.xlabel('Time')
+			plt.ylabel('Frequency')
+			plt.show()
+	else:
+		y, sr = librosa.load(filename,sr=None)
+		fmax_ = sr/2
+		S = librosa.feature.melspectrogram(y=y, sr=sr,fmax=fmax_)
+		spectrum = S
+		freqs = librosa.core.mel_frequencies(n_mels=S.shape[0],fmax=fmax_)
+		if(plot):	
+			plt.figure(figsize=(20, 8))
+			librosa.display.specshow(librosa.power_to_db(S,ref=np.max),y_axis='mel',fmax=fmax_,x_axis='time')
+			plt.colorbar(format='%+2.0f dB')
+			plt.title('Mel spectrogram')
+			plt.tight_layout()
+			plt.show()
 	return spectrum,freqs
 
 def write_spectro(spectrum,freqs,filepath,norm=True):
 	mean_amps = spectrum.mean(axis=1)
-	
 	if(norm):
 		simple_spectro = np.array([freqs,normalize(mean_amps)]).transpose()
 	else:
 		simple_spectro = np.array([freqs,mean_amps]).transpose()
 	
 	df = pd.DataFrame(data=simple_spectro,index=None)
+	print("Writing to file ",filepath,".....")
 	df.to_csv(filepath, sep=',', index=False,header=False)
+	print("File written successfully!")
+
+def write_peaks(peak_freqs, peak_amps,filepath,norm=True):
+	if(norm):
+		freq_amp = np.array([peak_freqs,normalize(peak_amps)]).transpose()
+	else:
+		freq_amp = np.array([peak_freqs,peak_amps]).transpose()
+	df = pd.DataFrame(data=freq_amp,index=None)
+	print("Writing to file ", filepath,"....")
+	df.to_csv(filepath,sep=',',index=False,header=False)
+	print("File written successfully!")
 
 def normalize(values,norm_0=True):
 	_min = values.min()
@@ -47,3 +74,19 @@ def normalize(values,norm_0=True):
 	else:
 		norm_values = (_values - _min)/(_max - _min)
 	return norm_values
+# Spectrogram S (shape=(n_freqs,n_time)), frequencies freqs (shape=(n_freqs))
+# Returns freqs,amps
+# freqs = list of frequencies of each peak (shape=(n_peaks))
+# amps = mean amplitudes of corresponding peak frequencies (shape=(n_peaks))
+def get_peaks(S,freqs,plot=False):
+	S_db = librosa.power_to_db(S,ref=np.max)
+	means_db = S_db.mean(axis=1)
+	means_0 = means_db - means_db.min()
+	peaks_idx, _ = find_peaks(means_0,prominence=MIN_PROMINENCE, height=0)
+	if(plot):	
+		plt.figure(figsize=(20,8))
+		plt.plot(freqs,means_db)
+		plt.scatter(freqs[peaks_idx],means_db[peaks_idx])
+		plt.show()
+	means = S.mean(axis=1)
+	return freqs[peaks_idx],means[peaks_idx]
